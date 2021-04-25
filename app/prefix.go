27062ad2ccde7b22be6b6ad0848abe334c0ca949
zaketac <zaketac@gmail.com>
2021-04-25 13:23:35 +0200

    devnet1

---
+++ app/prefix.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+        sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+const (
+        AccountAddressPrefix = "rack"
+)
+
+var (
+        AccountPubKeyPrefix    = AccountAddressPrefix + "pub"
+        ValidatorAddressPrefix = AccountAddressPrefix + "valoper"
+        ValidatorPubKeyPrefix  = AccountAddressPrefix + "valoperpub"
+        ConsNodeAddressPrefix  = AccountAddressPrefix + "valcons"
+        ConsNodePubKeyPrefix   = AccountAddressPrefix + "valconspub"
+)
+
+func SetConfig() {
+        config := sdk.GetConfig()
+        config.SetBech32PrefixForAccount(AccountAddressPrefix, AccountPubKeyPrefix)
+        config.SetBech32PrefixForValidator(ValidatorAddressPrefix, ValidatorPubKeyPrefix)
+        config.SetBech32PrefixForConsensusNode(ConsNodeAddressPrefix, ConsNodePubKeyPrefix)
+        config.Seal()
+}
+
